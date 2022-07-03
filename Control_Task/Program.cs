279a0123aa7152e16f2940196f0813d32c308c39
Sergey Kasitsin <kasitsinsergey@gmail.com>
2022-07-03 15:10:43 +0300

    First working version

--- Control_Task/Program.cs
+++ Control_Task/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+// длина которых мнеьше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры, 
+// либо задать на старте выполнения алгоритма. При решениее не рекоменжуется пользоваться коллекциями, 
+// лучше обойтись исключительно массивами. 
+
+string[] StringArray = new String[] { "GB", "HappyNewYear", "1", "222", "GeekBrain", "Exam" };
+int count = 0; 
+
+for (int i = 0; i < StringArray.Length; i++)
+    {
+        if (StringArray[i].Length < 3 );
+        count++;
+    }
+
+string[] NewStringArray = new String[count]; 
+
+for (int i = 0; i < StringArray.Length; i++)
+    {
+        if (StringArray[i].Length <= 3 ) NewStringArray[i] = StringArray[i];
+        Console.Write($"{NewStringArray[i]} ");
+    }
+
+
