e5327841a17486f0576de64c7ab90d2adcfeac65
Sergey Kasitsin <kasitsinsergey@gmail.com>
2022-07-03 17:15:56 +0300

    final working version

--- Control_Task/Program.cs
+++ Control_Task/Program.cs
@@ -1,23 +1,35 @@
-﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+﻿
+// Написать программу, которая из имеющегося массива строк формирует массив из строк, 
 // длина которых мнеьше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры, 
 // либо задать на старте выполнения алгоритма. При решениее не рекоменжуется пользоваться коллекциями, 
 // лучше обойтись исключительно массивами. 
 
-string[] StringArray = new String[] { "GB", "HappyNewYear", "1", "222", "GeekBrain", "Exam" };
-int count = 0; 
+Console.WriteLine("Введите количество элементов в массиве: ");
+int k = int.Parse(Console.ReadLine());
 
-for (int i = 0; i < StringArray.Length; i++)
+string[] StringArray = new String[k];
+int count = 0;
+
+for (int i = 0; i < k; i++)
+{
+    Console.WriteLine("Введите элемент массива: ");
+    StringArray[i] = Console.ReadLine();
+    if (StringArray[i].Length < 3) ;
+    count++;
+}
+
+string[] NewStringArray = new String[count];
+
+int j = 0;
+{
+    for (int i = j; i < k; i++)
     {
-        if (StringArray[i].Length < 3 );
-        count++;
+        if (StringArray[i].Length <= 3)
+        {
+            NewStringArray[j] = StringArray[i];
+            Console.Write($"{NewStringArray[j]}; ");
+            j++;
+        }
     }
-
-string[] NewStringArray = new String[count]; 
-
-for (int i = 0; i < StringArray.Length; i++)
-    {
-        if (StringArray[i].Length <= 3 ) NewStringArray[i] = StringArray[i];
-        Console.Write($"{NewStringArray[i]} ");
-    }
-
+}
 
